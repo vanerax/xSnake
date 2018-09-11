013407cfcec5a2d60e079313ab03f3baa6cc42a7
Ricky Zhuang <ricky.zhuang@oracle.com>
2018-09-11 17:58:18 +0800

    init

---
+++ xSnake.js
@@ -0,0 +1,12 @@
+process.stdout.write("hello: ");
+process.stdout.cursorTo(0);
+process.stdout.write("world");
+
+process.stdout.write("\bbc");
+
+const MAP_X = 10;
+const MAP_Y = 10;
+for (var i=0;i<MAP_Y.length;i++) {
+   var sOutput = "";
+
+}
