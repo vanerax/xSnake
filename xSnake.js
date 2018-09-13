d7638260748a2a5fd7bff30e7096378d809c8e35
Ricky Zhuang <ricky.zhuang@oracle.com>
2018-09-13 17:57:03 +0800

    refactor

--- xSnake.js
+++ xSnake.js
@@ -1,26 +1,4 @@
-// process.stdout.write("hello: ");
-// process.stdout.cursorTo(0);
-// process.stdout.write("world");
-
-// process.stdout.write("a\n\r\b\bbc");
-
-// const MAP_X = 10;
-// const MAP_Y = 2;
-// var aOutput = [];
-// for (var y=0;y<MAP_Y;y++) {
-//    var sLine = Array(MAP_X).fill("#").join("");
-//    aOutput.push(sLine + "\n");
-// }
-// process.stdout.write(aOutput.join(""));
-
-// aOutput = [];
-// process.stdout.cursorTo(0);
-// process.stdout.write("\b");
-// for (var y=0;y<MAP_Y;y++) {
-//    var sLine = Array(MAP_X).fill(".").join("");
-//    aOutput.push(sLine + "\n");
-// }
-// process.stdout.write(aOutput.join(""));
+"use strict";
 
 var Direction = {
    UP: 38,
@@ -29,24 +7,32 @@ var Direction = {
    LEFT: 37
 };
 
-var MAP_X = 10;
-var MAP_Y = 10;
+var KeyCodeToDirection = {
+   38: Direction.UP,
+   39: Direction.RIGHT,
+   40: Direction.DOWN,
+   37: Direction.LEFT
+};
 
 function SnakeModel() {
    var _oSnakeModel = this;
 
+   this.cellUnit = 32;
+   this.mapWidth = 20;
+   this.mapHeight = 10;
+
    var _oSnake = {
       head: {
-         posX: Math.floor((MAP_X - 1) / 2),
-         posY: Math.floor((MAP_X - 1) / 2),
+         posX: Math.floor((this.mapWidth - 1) / 2),
+         posY: Math.floor((this.mapHeight - 1) / 2),
          direction: Direction.UP
       },
       body: [{
-         posX: Math.floor((MAP_X - 1) / 2),
-         posY: Math.floor((MAP_X - 1) / 2) + 1
+         posX: Math.floor((this.mapWidth - 1) / 2),
+         posY: Math.floor((this.mapHeight - 1) / 2) + 1
       }, {
-         posX: Math.floor((MAP_X - 1) / 2),
-         posY: Math.floor((MAP_X - 1) / 2) + 2
+         posX: Math.floor((this.mapWidth - 1) / 2),
+         posY: Math.floor((this.mapHeight - 1) / 2) + 2
       }]
    };
 
@@ -73,11 +59,72 @@ function SnakeModel() {
       }
    });
 
-   this.onModelChange = function() {
-
-   };
+   this.onModelChange = function() { };
 
    this.onKeyDown = function(oEvent) {
+      var eDir = KeyCodeToDirection[oEvent.keyCode];
+      if (eDir && this.isReachable(eDir)) {
+         this.move(eDir);
+      }
+   };
+
+   this.getHeadPosition = function() {
+      return {
+         x: this.snake.head.posX,
+         y: this.snake.head.posY
+      };
+   };
+
+   this.getNextPosition = function(eDirection, oCurPos) {
+      var oNextPos = {
+         x: oCurPos.x,
+         y: oCurPos.y
+      };
+
+      switch (eDirection) {
+         case Direction.UP:
+            oNextPos.y -= 1;
+            break;
+
+         case Direction.RIGHT:
+            oNextPos.x += 1;
+            break;
+
+         case Direction.DOWN:
+            oNextPos.y += 1;
+            break;
+
+         case Direction.LEFT:
+            oNextPos.x -= 1;
+            break;
+      }
+      return oNextPos;
+   };
+
+   this.getReverseDirection = function(eDirection) {
+      var eRet;
+      switch (eDirection) {
+         case Direction.UP:
+            eRet = Direction.DOWN;
+            break;
+
+         case Direction.RIGHT:
+            eRet = Direction.LEFT;
+            break;
+
+         case Direction.DOWN:
+            eRet = Direction.UP;
+            break;
+
+         case Direction.LEFT:
+            eRet = Direction.RIGHT;
+            break;
+      }
+      return eRet;
+   };
+
+   this.move = function(eDirection) {
+      var oNextPos = this.getNextPosition(eDirection, this.getHeadPosition());
       var _fMoveBody = function() {
          var oBody = _oSnakeModel.snake.body;
          for (var i=oBody.length-1; i>0; i--) {
@@ -91,81 +138,123 @@ function SnakeModel() {
          }
       };
 
-      switch (oEvent.keyCode) {
-         case 37: // left
-            if (_oSnakeModel.snake.head.posX > 0) {
-               _fMoveBody();
-               _oSnakeModel.snake.head.posX = _oSnakeModel.snake.head.posX - 1;
-            }
+      _fMoveBody();
+      _oSnakeModel.snake.head.posX = oNextPos.x;
+      _oSnakeModel.snake.head.posY = oNextPos.y;
+      _oSnakeModel.snake.head.direction = eDirection;
+   };
+
+   this.isReachable = function(eDirection) {
+      var oPos = this.getNextPosition(eDirection, this.getHeadPosition());
+      return this.isInMap(oPos) && !this.isSelfOccupied(oPos);
+   };
+
+   this.isCellAvailable = function(oPos) {
+      return this.isInMap(oPos) && !this.isSelfOccupied(oPos);
+   };
+
+   this.isInMap = function(oPos) {
+      return oPos.x >= 0 && oPos.x < this.mapWidth && oPos.y >= 0 && oPos.y < this.mapHeight;
+   };
+
+   this.isSelfOccupied = function(oPos) {
+      var bRet = false;
+      for (var i=0;i<this.snake.body.length;i++) {
+         var oBody = this.snake.body[i];
+         if (oBody.posX === oPos.x && oBody.posY === oPos.y) {
+            bRet = true;
             break;
-         case 38: // up
-            if (_oSnakeModel.snake.head.posY > 0) {
-               _fMoveBody();
-               _oSnakeModel.snake.head.posY = _oSnakeModel.snake.head.posY - 1;
-            }
-            break;
-         case 39: // right
-            if (_oSnakeModel.snake.head.posX < MAP_X - 1) {
-               _fMoveBody();
-               _oSnakeModel.snake.head.posX = _oSnakeModel.snake.head.posX + 1;
-            }
-            break;
-         case 40: // down
-            if (_oSnakeModel.snake.head.posY < MAP_Y - 1) {
-               _fMoveBody();
-               _oSnakeModel.snake.head.posY = _oSnakeModel.snake.head.posY + 1;
-            }
-            break;
+         }
+      }
+      return bRet;
+   };
+
+   this.addTail = function() {
+      if (this.snake.body.length === 0) {
+         var eDir = this.getReverseDirection(this.snake.head.direction);
+         var oPos = this.getNextPosition(eDir, this.getHeadPosition());
+         this.snake.body.add({
+            posX: oPos.x,
+            posY: oPos.y
+         });
+      } else if (this.snake.body.length === 1) {
+         //this.snake.body[0]
+      } else {
+         // this.snake.body[this.snake.body.length-1];
+      }
+   };
+}
+
+window.onload = function() {
+   var renderLayout = function(elCanvas, oSnakeModel) {
+      elCanvas.style.width = oSnakeModel.mapWidth * oSnakeModel.cellUnit + "px";
+
+      for (var y=0; y<oSnakeModel.mapHeight; y++) {
+         for (var x=0; x<oSnakeModel.mapWidth; x++) {
+            var elCell = document.createElement("div");
+            elCell.classList.add("cell");
+            elCell.classList.add("cell_" + y + "_" + x);
+            elCell.classList.add("row_" + y);
+            elCell.classList.add("col_" + x);
+            elCanvas.appendChild(elCell);
+
+            var oContext = {
+               posX: x,
+               posY: y,
+               snakeModel: oSnakeModel
+            };
+            renderSnakeCell(elCell, oContext);
+         }
       }
    };
 
-
-}
-
-
-
-this.renderLayout = function(elCanvas) {
-   for (var y=0; y<MAP_Y; y++) {
-      for (var x=0; x<MAP_X; x++) {
-         var elCell = document.createElement("div");
-         elCell.classList.add("cell");
-         elCell.classList.add("cell_" + y + "_" + x);
-         elCell.classList.add("row_" + y);
-         elCell.classList.add("col_" + x);
-         elCanvas.appendChild(elCell);
+   var renderSnake = function(elCanvas, oSnakeModel) {
+      var elCells = elCanvas.children;
+      for (var i=0; i<elCells.length; i++) {
+         elCells[i].innerHTML = "";
       }
-   }
-};
 
-this.renderSnake = function(elCanvas, oSnakeModel) {
-   var elCells = elCanvas.children;
-   for (var i=0; i<elCells.length; i++) {
-      elCells[i].innerHTML = "";
-   }
+      var x = oSnakeModel.snake.head.posX;
+      var y = oSnakeModel.snake.head.posY;
+      var elHead = document.createElement("div");
+      elHead.classList.add("snake-head");
+      elCells[oSnakeModel.mapWidth*y+x].appendChild(elHead);
 
-   var x = oSnakeModel.snake.head.posX;
-   var y = oSnakeModel.snake.head.posY;
-   var elHead = document.createElement("div");
-   elHead.classList.add("snake-head");
-   elCells[MAP_X*y+x].appendChild(elHead);
+      for (var j=0; j<oSnakeModel.snake.body.length; j++) {
+         var nCurPosX = oSnakeModel.snake.body[j].posX;
+         var nCurPosY = oSnakeModel.snake.body[j].posY;
+         var elBody = document.createElement("div");
+         elBody.classList.add("snake-body");
+         elCells[oSnakeModel.mapWidth*nCurPosY+nCurPosX].appendChild(elBody);
+      }
+   };
 
-   for (var j=0; j<oSnakeModel.snake.body.length; j++) {
-      var nCurPosX = oSnakeModel.snake.body[j].posX;
-      var nCurPosY = oSnakeModel.snake.body[j].posY;
-      var elBody = document.createElement("div");
-      elBody.classList.add("snake-body");
-      elCells[MAP_X*nCurPosY+nCurPosX].appendChild(elBody);
-   }
-};
+   var renderSnakeCell = function(elCell, oContext) {
+      var oSnakeModel = oContext.snakeModel;
+      if (oSnakeModel.snake.head.posX === oContext.posX && oSnakeModel.snake.head.posY === oContext.posY) {
+         var elHead = document.createElement("div");
+         elHead.classList.add("snake-head");
+         elCell.appendChild(elHead);
+      } else {
+         for (var i=0;i<oSnakeModel.snake.body.length;i++) {
+            var oBody = oSnakeModel.snake.body[i];
+            if (oBody.posX === oContext.posX && oBody.posY === oContext.posY) {
+               var elBody = document.createElement("div");
+               elBody.classList.add("snake-body");
+               elBody.classList.add("snake-body-" + i);
+               elCell.appendChild(elBody);
+            }
+         }
+      }
+   };
 
-window.onload = function() {
    var oSnakeModel = new SnakeModel();
    var elCanvas = document.getElementById("canvas");
-   renderLayout(elCanvas);
-   renderSnake(elCanvas, oSnakeModel);
+   renderLayout(elCanvas, oSnakeModel);
+   //renderSnake(elCanvas, oSnakeModel);
 
    oSnakeModel.onModelChange = function() {
       renderSnake(elCanvas, oSnakeModel);
    };
-   document.body.addEventListener('keydown', oSnakeModel.onKeyDown);
+   document.body.addEventListener('keydown', oSnakeModel.onKeyDown.bind(oSnakeModel));
 };
